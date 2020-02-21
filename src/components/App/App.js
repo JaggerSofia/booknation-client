4f6171cf117c0b4ac487d5ef99f9fc19d88dc658
Victoria Moore <victoriamoore563@gmail.com>
2020-02-20 19:38:48 -0800

    content added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Header from '../Header/Header'
+import PrivateRoute from '../Utils/PrivateRoute'
+import PublicOnlyRoute from '../Utils/PublicOnlyRoute'
+import BookListPage from '../../routes/BookListPage/BookListPage'
+import BookPage from '../../routes/BookPage/BookPage'
+import LoginPage from '../../routes/LoginPage/LoginPage'
+import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage'
+import NotFoundPage from '../../routes/NotFoundPage/NotFoundPage'
+import './App.css'
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends Component {
+  state = { hasError: false }
+
+  static getDerivedStateFromError(error) {
+    console.error(error)
+    return { hasError: true }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App__header'>
+          <Header />
+        </header>
+        <main className='App__main'>
+          {this.state.hasError && <p className='red'>There was an error! Oh no!</p>}
+          <Switch>
+            <Route
+              exact
+              path={'/'}
+              component={BookListPage}
+            />
+            <PublicOnlyRoute path={'/login'} component={LoginPage} />
+            <PublicOnlyRoute path={'/register'} component={RegistrationPage} />
+            <PrivateRoute path={'/book/:bookId'} component={BookPage} />
+            <Route
+              component={NotFoundPage}
+            />
+          </Switch>
+        </main>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
