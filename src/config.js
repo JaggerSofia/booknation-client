aa25b4ebb51237e2be42f3c1aea82be5b19f0f8c
Victoria Moore <victoriamoore563@gmail.com>
2020-04-22 19:54:08 -0700

    / removed

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-     API_ENDPOINT: 'https://shrouded-wave-72208.herokuapp.com/api/',
+     API_ENDPOINT: 'https://shrouded-wave-72208.herokuapp.com/api',
      TOKEN_KEY: 'booknation-client-auth-token',
    }
