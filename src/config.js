6cddc5dedc238758a730ae7da77b1b3af1478774
Victoria Moore <victoriamoore563@gmail.com>
2020-04-22 19:28:13 -0700

    connected backend

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-     API_ENDPOINT: 'http://localhost:8000/api',
+     API_ENDPOINT: 'https://shrouded-wave-72208.herokuapp.com/',
      TOKEN_KEY: 'booknation-client-auth-token',
    }
