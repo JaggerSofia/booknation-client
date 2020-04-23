e9247144cf7995d6e650efed540fa57131bfe197
Victoria Moore <victoriamoore563@gmail.com>
2020-04-23 00:41:53 -0700

    added logout button when signed in

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -52,4 +52,54 @@ export default class Header extends Component {
       <span className='Header__tagline--narrow'>Rate all the books.</span>
     </>
   }
+}export default class Header extends Component {
+  handleLogoutClick = () => {
+    TokenService.clearAuthToken()
+  }
+
+  renderLogoutLink() {
+    return (
+      <div className='Header__logged-in'>
+        <Link
+          onClick={this.handleLogoutClick}
+          to='/'>
+          Logout
+        </Link>
+      </div>
+    )
+  }
+
+  renderLoginLink() {
+    return (
+      <div className='Header__not-logged-in'>
+        <Link
+          to='/login'>
+          Log in
+        </Link>
+        <Link
+          to='/register'>
+          Register
+        </Link>
+      </div>
+    )
+  }
+
+  render() {
+    return <>
+      <nav className='Header'>
+        <h1>
+          <Link to='/'>
+            {' '}
+            BookNation
+          </Link>
+        </h1>
+        <span className='Header__tagline--wide'>Rate all the books.</span>
+        {TokenService.hasAuthToken()
+          ? this.renderLogoutLink()
+          : this.renderLoginLink()}
+      </nav>
+
+      <span className='Header__tagline--narrow'>Rate all the books.</span>
+    </>
+  }
 }
