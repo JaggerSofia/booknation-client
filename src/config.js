9a9ed3d74ae01ad72ba80d2e75deb5c3b09d5d13
Victoria Moore <victoriamoore563@gmail.com>
2020-04-23 01:12:04 -0700

    connect to server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-     API_ENDPOINT: 'https://git.heroku.com/shrouded-wave-72208.git',
+     API_ENDPOINT: 'https://git.heroku.com/shrouded-wave-72208.git/api',
      TOKEN_KEY: 'booknation-client-auth-token',
    }
