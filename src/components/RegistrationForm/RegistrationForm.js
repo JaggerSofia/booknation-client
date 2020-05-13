96daab7f52167307672c8a48b36da82920d8db4a
Victoria Moore <victoriamoore@Victorias-MacBook-Pro.local>
2020-05-13 13:41:23 -0700

    token service removed

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import TokenService from '../../services/token-service'
 import AuthApiService from '../../services/auth-api-service'
 import { Button, Input, Required } from '../Utils/Utils'
 
